f51abbbc8751e1fff658e485913fcd7c3d636d08
ohjisu320 <ohjisu320@gmail.com>
2024-01-30 14:25:26 +0900

    commit

---
+++ docs/quests/DDA/SpineSurgeryList_apply.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import SpineSurgeryList_apply as csv
+
+df_SSL = pd.read_csv('./docs/quests/CSVs/SpineSurgeryList.csv')
+
+def cal_bmi(params):
+  result =params['체중']/(params['신장']*0.1)**2
+  return round(result*100, 1)
+
+df_SSL['bmi(%)']=df_SSL[["신장", "체중"]].apply(cal_bmi,  axis=1)
+pass
+
+print(df_SSL[['체중', '신장', 'bmi(%)']])
+
+def surgery_hours(params):
+    try : 
+        result =int(params//60)
+    except :
+        result = 'nan'
+    return result
+df_SSL['수술시간_datetime_hours']=df_SSL['수술시간'].apply(surgery_hours)
+pass
+
+def surgery_minutes(params):
+    try : 
+        result =int(params%60)
+    except :
+        result = 'nan'
+    return result
+df_SSL['수술시간_datetime_minutes']=df_SSL['수술시간'].apply(surgery_minutes)
+
+df_SSL[['수술시간', '수술시간_datetime_hours', '수술시간_datetime_minutes']]
+pass
